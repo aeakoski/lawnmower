da36195605b674b6957bc318df18d17effcd140e
akoski <akoski@kth.se>
2022-05-20 09:49:26 +0200

    wip

--- socket_client.py
+++ socket_client.py
@@ -4,6 +4,7 @@ import socket
 import tty
 import sys
 import termios
+import keyboard
 
 def client_program():
     x = 0
@@ -14,9 +15,22 @@ def client_program():
     client_socket.connect((host, port))  # connect to the server
 
     while True:
-        x=sys.stdin.read(1)[0]
-        if x == chr(27): # ESC
+        if keyboard.is_pressed('w'):  # if key 'q' is pressed
+            x= "w"
+            
+        elif keyboard.is_pressed('a'):  # if key 'q' is pressed
+            x= "a"
+        elif keyboard.is_pressed('s'):  # if key 'q' is pressed
+            x= "s"
+        elif keyboard.is_pressed('d'):  # if key 'q' is pressed
+            x= "d"
+        elif keyboard.is_pressed('q'):  # if key 'q' is pressed
+            x= "q"
             break
+
+        #x=sys.stdin.read(1)[0]
+        #if x == chr(27): # ESC
+        #    break
         client_socket.send(x.encode())  # send message
 
         print("You pressed", x)
