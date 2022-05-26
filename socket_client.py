36606c9e3b7d9742dd9ed7d40d451a400e13373a
akoski <akoski@kth.se>
2022-05-26 17:25:22 +0200

    Better keyboard sencing logic

--- socket_client.py
+++ socket_client.py
@@ -5,19 +5,24 @@ import tty
 import sys
 import termios
 import keyboard
+import time
 
 def client_program():
     x = 0
+    lastMessage = " "
     host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
-
+    print("Connected")
     while True:
+        time.sleep(0.01)
+
+        x = " "
+
         if keyboard.is_pressed('w'):  # if key 'q' is pressed
             x= "w"
-            
         elif keyboard.is_pressed('a'):  # if key 'q' is pressed
             x= "a"
         elif keyboard.is_pressed('s'):  # if key 'q' is pressed
@@ -27,13 +32,14 @@ def client_program():
         elif keyboard.is_pressed('q'):  # if key 'q' is pressed
             x= "q"
             break
+        print("You pressed", x, end="\r")
 
-        #x=sys.stdin.read(1)[0]
-        #if x == chr(27): # ESC
-        #    break
-        client_socket.send(x.encode())  # send message
+        if lastMessage == " " and x == " ":
+            pass
+        else:
+            client_socket.send(x.encode())  # send message
+            lastMessage = x
 
-        print("You pressed", x)
         # TODO Make it so all new keypresses overwrite the previous one in the cosole
 
     client_socket.close()  # close the connection
@@ -47,6 +53,8 @@ if __name__ == '__main__':
     try:
         client_program()
 
-    except:
+    except Exception as e:
         print("Exiting")
+        print(e)
+
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
